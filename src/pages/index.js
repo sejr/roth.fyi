8cea8bb27456c531c23bc3f54faf42bc7a6ebe0f
Sam Jackson <sam@clique.app>
2018-10-31 16:41:03 -0400

    Email list form and icon font added

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,39 +53,17 @@ export default class IndexPage extends React.Component {
               </Link>
             ))
           }
-        </div>
-
-        {/* <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+          <div className="mailing-list">
+            <span class="icon-at"></span>
+            <div className="action">
+              <p className="call-to-action">Let's keep in touch.</p>
+              <p className="call-description">
+                Join my mailing list to get new posts straight in your inbox.
+              </p>
             </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
+            <input className="action-email" placeholder="john@example.com" />
           </div>
-        </section> */}
+        </div>
       </Layout>
     )
   }
