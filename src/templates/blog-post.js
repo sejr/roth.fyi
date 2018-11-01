8746fe0e7c2822615fcee10d2d74c6353b79b0db
Sam Jackson <sam@clique.app>
2018-11-01 12:14:03 -0400

    Initial post layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,39 +8,40 @@ import Content, { HTMLContent } from '../components/Content'
 
 export const BlogPostTemplate = ({
   content,
+  date,
   contentComponent,
   description,
   tags,
   title,
   helmet,
+  image
 }) => {
   const PostContent = contentComponent || Content
 
   return (
-    <section className="section">
+    <section className="blog">
       {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
+      <img className="blog-image" alt={title} src={image} />
+
+      <div class="blog-content">
+        <p className="blog-date">{date}</p>
+        <h1 className="blog-title">{title}</h1>
+        <p className="blog-description">{description}</p>
+
+        <PostContent content={content} />
+
+        {tags && tags.length ? (
+          <div style={{ marginTop: `4rem` }}>
+            <h4>Tags</h4>
+            <ul className="taglist">
+              {tags.map(tag => (
+                <li key={tag + `tag`}>
+                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </li>
+              ))}
+            </ul>
           </div>
-        </div>
+        ) : null}
       </div>
     </section>
   )
@@ -66,6 +67,8 @@ const BlogPost = ({ data }) => {
         helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
         tags={post.frontmatter.tags}
         title={post.frontmatter.title}
+        date={post.frontmatter.date}
+        image={post.frontmatter.featuredImage}
       />
     </Layout>
   )
@@ -85,6 +88,7 @@ export const pageQuery = graphql`
       id
       html
       frontmatter {
+        featuredImage
         date(formatString: "MMMM DD, YYYY")
         title
         description
